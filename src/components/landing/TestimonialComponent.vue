651f12d0e2e2e57aafdae16242742ec752839a50
Success Ahon <40746773+arudovwen@users.noreply.github.com>
2023-06-23 00:46:31 +0100

    update

--- src/components/landing/TestimonialComponent.vue
+++ src/components/landing/TestimonialComponent.vue
@@ -4,20 +4,22 @@
       class="grid grid-cols-1 lg:grid-cols-2 px-6 sm:px-8 lg:px-16 border-t border-b border-[#a18463]/20 mb-10 mt-20 py-16 lg:py-0"
     >
       <div class="relative lg:border-r border-[#a18463]/20 mb-4 lg:mb-0">
-        <carousel :modelValue="slideIndex" :items-to-show="1" ref="mainSlide">
+        <carousel
+          :autoplay="4000"
+          :modelValue="slideIndex"
+          :items-to-show="1"
+          ref="mainSlide"
+        >
           <slide
             class="flex flex-col text-left items-start"
-            v-for="slide in 6"
-            :key="slide"
+            v-for="(slide, i) in testimonials"
+            :key="i"
           >
             <div
               class="w-full h-[500px] py-10 pr-10 hidden lg:block overflow-hidden"
               v-animate-onscroll="'scale-in-center'"
             >
-              <img
-                src="@/assets/images/testv.jpeg"
-                class="w-full h-full transition-all duration-1000 hover:scale-105"
-              />
+              <img :src="slide.img" class="w-full h-full object-contain" />
             </div>
           </slide>
         </carousel>
@@ -27,25 +29,36 @@
         >
       </div>
 
-      <div class="pl-8 p-6 lg:p-16 relative">
-        <span class="absolute z-[1] top-4 left-6 opacity-40 text-8xl">"</span>
-        <carousel :modelValue="slideIndex" :items-to-show="1" ref="sideSlide">
-          <slide class="justify-start" v-for="slide in 10" :key="slide">
-            <div class="max-w-[400px] text-left z-[2] relative">
+      <div class="pl-8 p-6 lg:p-10 relative flex flex-col justify-center">
+        <span class="absolute z-[1] top-4 left-6 opacity-20">
+          <i class="fa fa-quote-left fa-5x quote" aria-hidden="true"></i
+        ></span>
+        <span class="absolute z-[1] bottom-4 right-6 opacity-20">
+          <i class="fa fa-quote-right fa-5x quote" aria-hidden="true"></i
+        ></span>
+        <carousel
+          :autoplay="4000"
+          :modelValue="slideIndex"
+          :items-to-show="1"
+          ref="sideSlide"
+        >
+          <slide
+            class="justify-start"
+            v-for="(slide, i) in testimonials"
+            :key="i"
+          >
+            <div class="text-left z-[2] relative">
               <p
-                class="text-sm lg:text-2xl font-semibold mb-6"
+                class="text-sm lg:text-xl font-semibold mb-6"
                 v-animate-onscroll="'fade-in-bottom'"
               >
-                We had the best time of our lives enjoying our honeymoon at the
-                Luxury apartment. It was like our first home as a couple. The
-                environment is secure and we had every single thing we needed
-                from the living room to the kitchen.
+                {{ slide.text }}
               </p>
               <p
                 class="text-sm lg:text-lg font-medium"
                 v-animate-onscroll="'fade-in-bottom'"
               >
-                Obinna & Kate Nwachukwu
+                {{ slide.title }}
               </p>
               <div
                 class="w-[100px] my-1 sm:my-2 border"
@@ -55,7 +68,7 @@
                 class="text-xs lg:text-sm text-green-700 font-medium"
                 v-animate-onscroll="'fade-in-bottom'"
               >
-                May 2022
+                {{ slide.date }}
               </p>
             </div>
           </slide>
@@ -88,6 +101,26 @@ import { ref } from "vue";
 const mainSlide = ref(null);
 const sideSlide = ref(null);
 const slideIndex = ref(0);
+const testimonials = [
+  {
+    title: "Zinny Michael",
+    text: "You glow differently when relaxing in the right location pampered with vamble staff",
+    img: require("@/assets/images/testimonial.jpg"),
+    date: "May 21",
+  },
+  {
+    title: "Sadiq Nuhu",
+    text: "My stay was comfortable throughout the two weeks I spent. I would definitely patronize Vamble Apartments and Suites the next time I come to Nigeria.",
+    img: require("@/assets/images/testi1.jpg"),
+    date: "Dec 18",
+  },
+  {
+    title: "Obinna & Kate Nwachukwu",
+    text: "We had the best time of our lives enjoying our honeymoon at the Luxury apartment. It was like our first home as a couple. The environment is secure and we had every single thing we needed from the living room to the kitchen.",
+    img: require("@/assets/images/testi.jpg"),
+    date: "Sept 18",
+  },
+];
 function handleNext() {
   mainSlide.value.next();
   sideSlide.value.next();
